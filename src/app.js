64b80441e6161453156b192927a8f1e4da05eb49
Alefantozzi86 <alefantozzi86@gmail.com>
2023-02-26 00:09:10 +0000

    Random Card

--- src/app.js
+++ src/app.js
@@ -2,10 +2,50 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const simbol = ["♦", "♥", "♠", "♣"];
+const numbers = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomSimbol = Math.floor(Math.random() * simbol.length);
+
+  let randomNumber = Math.floor(Math.random() * numbers.length);
+
+  let simbolUp = document.querySelector(".simbol-up");
+  simbolUp.innerHTML = simbol[randomSimbol];
+
+  let simbolDown = document.querySelector(".simbol-down");
+  simbolDown.innerHTML = simbol[randomSimbol];
+
+  let cardValue = document.querySelector(".number");
+  cardValue.innerHTML = numbers[randomNumber];
+
+  if (randomSimbol <= 1) {
+    simbolUp.style.color = "red";
+    simbolDown.style.color = "red";
+    cardValue.style.color = "red";
+  }
+
+  const button = document.querySelector("#button");
+  function reload() {
+    reload = location.reload();
+  }
+
+  button.addEventListener("click", reload, false);
+
+  //setTimeout(reload, 2500);
 };
